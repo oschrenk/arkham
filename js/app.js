41a5c11732fbd1865ef83bbd4a5c06f255644733
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-07-23 20:40:08 +0200

    Prevent zooming on quick multi tap

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+// prevent iOS from zooming on quick multi taps
+var lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  var now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
+
 Vue.component('number-picker', {
   template: `
     <div class="counter">
